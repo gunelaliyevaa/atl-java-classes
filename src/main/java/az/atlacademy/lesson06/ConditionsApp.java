658380869260c0b740986348850ac3c0928a35df
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-07 20:14:40 +0400

    ConditionsApp created

---
+++ src/main/java/az/atlacademy/lesson06/ConditionsApp.java
@@ -0,0 +1,14 @@
+package az.atlacademy.lesson06;
+
+import java.util.Scanner;
+
+public class ConditionsApp {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int var = scan.nextInt();
+
+        if ( var < 0) System.out.println("negative!");
+        else System.out.println("positive!");
+    }
+}
