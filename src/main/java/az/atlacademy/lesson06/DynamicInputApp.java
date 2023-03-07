be043e7a39632f5c812d712207d7883056552d56
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-07 19:33:02 +0400

    DynamicInputApp created

---
+++ src/main/java/az/atlacademy/lesson06/DynamicInputApp.java
@@ -0,0 +1,14 @@
+package az.atlacademy.lesson06;
+
+import java.util.Scanner;
+
+public class DynamicInputApp {
+    public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+         double var = scan.nextDouble();
+
+        System.out.printf("%.5f",var);
+
+
+    }
+}
