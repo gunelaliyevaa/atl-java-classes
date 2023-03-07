55601c8d20008603c2aeea0631997d5380eb7f1f
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-07 13:14:02 +0400

    Lessons Updated

--- src/main/java/az/atlacademy/lesson02/Hello.java
+++
@@ -1,7 +0,0 @@
-package az.atlacademy.lesson02;
-public class Hello {
-    public static void main(String[] args) {
-
-        System.out.println("Hello Universe!!!");
-    }
-}
---
+++ src/main/java/az/atlacademy/lesson03/HelloWorldApp.java
@@ -0,0 +1,10 @@
+package az.atlacademy.lesson03;
+
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+
+}
