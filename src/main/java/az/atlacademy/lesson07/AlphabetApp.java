7561510b24c8b9afed7fb7772688d3495d922dfd
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-09 20:55:40 +0400

    SumOfDigitsApp created

--- src/main/java/az/atlacademy/lesson07/AlphabetApp.java
+++ src/main/java/az/atlacademy/lesson07/AlphabetApp.java
@@ -3,8 +3,8 @@ package az.atlacademy.lesson07;
 
 public class AlphabetApp {
     public static void main(String[] args) {
-        for (char i = 65; i <= 90; i++) {
-            System.out.println(i);
+        for (int i = 65; i <= 90; i++) {
+            System.out.println((char)i);
         }
     }
 }
