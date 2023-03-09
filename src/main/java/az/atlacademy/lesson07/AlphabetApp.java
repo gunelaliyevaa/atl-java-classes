acf6569e4e89b2ab8547c4e8862d15d9d63c5c75
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-09 20:19:37 +0400

    AlphabetApp created

---
+++ src/main/java/az/atlacademy/lesson07/AlphabetApp.java
@@ -0,0 +1,10 @@
+package az.atlacademy.lesson07;
+
+
+public class AlphabetApp {
+    public static void main(String[] args) {
+        for (char i = 65; i <= 90; i++) {
+            System.out.println(i);
+        }
+    }
+}
