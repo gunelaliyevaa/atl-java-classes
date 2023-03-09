f61a1d5159a5e946e0075e74da9bd1598158e886
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-09 19:24:19 +0400

    FizzBuzz solved

--- src/main/java/az/atlacademy/lesson06/FizzBuzz.java
+++ src/main/java/az/atlacademy/lesson06/FizzBuzz.java
@@ -7,14 +7,14 @@ public class FizzBuzz {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-            if (n % 3 == 0 && n % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (n % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (n % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(n);
+        if (n % 3 == 0 && n % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (n % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (n % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(n);
         }
     }
 }
