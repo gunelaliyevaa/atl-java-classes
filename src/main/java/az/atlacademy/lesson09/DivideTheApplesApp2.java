8b16641395905eeab9b5afd95862566824882583
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-13 20:34:52 +0400

    DivideTheApplesApp2 created

---
+++ src/main/java/az/atlacademy/lesson09/DivideTheApplesApp2.java
@@ -0,0 +1,13 @@
+package az.atlacademy.lesson09;
+
+import java.util.Scanner;
+
+public class DivideTheApplesApp2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int res = k % n;
+        System.out.println(res);
+    }
+}
