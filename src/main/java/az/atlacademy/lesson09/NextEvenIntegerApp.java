83ae68130f51f02e9f24110c4cea507c4bd76a84
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-14 02:16:17 +0400

    NextEvenIntegerApp created

---
+++ src/main/java/az/atlacademy/lesson09/NextEvenIntegerApp.java
@@ -0,0 +1,17 @@
+package az.atlacademy.lesson09;
+
+import java.util.Scanner;
+
+public class NextEvenIntegerApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println(n + 2);
+        } else {
+            System.out.println(n + 1);
+        }
+    }
+}
+
