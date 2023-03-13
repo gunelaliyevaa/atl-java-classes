ca8e1d57609dc0c4e7b08fa1ecc19d7dcd39efb9
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-13 20:36:38 +0400

    PrintSeparatelyApp created

---
+++ src/main/java/az/atlacademy/lesson09/PrintSeparatelyApp.java
@@ -0,0 +1,8 @@
+package az.atlacademy.lesson09;
+
+public class PrintSeparatelyApp {
+    public static void main(String[] args) {
+        System.out.printf("1\n" + "2\n" + "3\n" + "4\n" + "5\n");
+
+    }
+}
