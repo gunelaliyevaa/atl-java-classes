3e8f2d1d3d93e87afa554a03e47e93f95e94a2f1
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-13 20:33:54 +0400

    AreaOfRectangleApp created

---
+++ src/main/java/az/atlacademy/lesson09/AreaOfRectangleApp.java
@@ -0,0 +1,16 @@
+package az.atlacademy.lesson09;
+
+import java.util.Scanner;
+
+public class AreaOfRectangleApp {
+    public static void main(String[] args) {
+        Scanner parameter = new Scanner(System.in);
+        int a = parameter.nextInt();
+        int b = parameter.nextInt();
+
+        int area = a * b;
+        System.out.println(area);
+
+
+    }
+}
