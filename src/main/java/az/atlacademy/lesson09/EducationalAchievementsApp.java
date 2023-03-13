972c747b538af55a91f294895942f416532a5ee7
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-13 20:39:37 +0400

    SimpleProblemApp created

---
+++ src/main/java/az/atlacademy/lesson09/EducationalAchievementsApp.java
@@ -0,0 +1,21 @@
+package az.atlacademy.lesson09;
+
+import java.util.Scanner;
+
+public class EducationalAchievementsApp {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if (n >= 1 && n <= 3) {
+            System.out.println("Initial");
+        } else if (n >= 4 && n <= 6) {
+            System.out.println("Average");
+        } else if (n >= 7 && n <= 9) {
+            System.out.println("Sufficient");
+        } else if (n >= 10 && n <= 12) {
+            System.out.println("High");
+        }
+    }
+}
