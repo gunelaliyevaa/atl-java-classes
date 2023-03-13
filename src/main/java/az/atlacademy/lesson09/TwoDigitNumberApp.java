9c01bf55deb6dde856ffc9827963aded986d5bf0
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-14 01:48:08 +0400

    TwoDigitNumberApp created

---
+++ src/main/java/az/atlacademy/lesson09/TwoDigitNumberApp.java
@@ -0,0 +1,16 @@
+package az.atlacademy.lesson09;
+
+import java.util.Scanner;
+
+public class TwoDigitNumberApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        if ((n < 100 && n > 9) || (n > -100 && n < -9)) {
+            System.out.println("Ok");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
