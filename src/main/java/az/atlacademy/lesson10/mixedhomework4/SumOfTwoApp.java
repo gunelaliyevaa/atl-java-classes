03742e43e5e343ac512186cd729415a53dba2e7f
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-25 02:15:21 +0400

    SumOfTwoApp created

---
+++ src/main/java/az/atlacademy/lesson10/mixedhomework4/SumOfTwoApp.java
@@ -0,0 +1,16 @@
+package az.atlacademy.lesson10.mixedhomework4;
+
+import java.util.Scanner;
+
+public class SumOfTwoApp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
