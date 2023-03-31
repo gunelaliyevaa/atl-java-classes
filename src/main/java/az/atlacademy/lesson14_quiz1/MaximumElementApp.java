2e7dfb7103c66b5f77a18e11f2d5e96e80260bd8
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-03-31 21:18:26 +0400

    MaximumElementApp created

--- src/main/java/az/atlacademy/lesson14_quiz1/MaximumElementApp.java
+++ src/main/java/az/atlacademy/lesson14_quiz1/MaximumElementApp.java
@@ -7,17 +7,16 @@ public class MaximumElementApp {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
-        for (int i = n; i >= 1; i--) {
-            for (int j = i; j >= 1; j--) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
         System.out.println();
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
