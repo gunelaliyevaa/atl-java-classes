04fc2d9ad03448940422541b199904b68a6d0849
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-04-24 21:20:48 +0400

    Student class created

---
+++ src/main/java/az/atlacademy/module01/lesson21/Student.java
@@ -0,0 +1,24 @@
+package az.atlacademy.module01.lesson21;
+
+public class Student {
+
+    private final int age;
+    private final double[] grades;
+
+    public Student(int age, double[] grades) {
+        this.age = age;
+        this.grades = grades;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public final double[] getGrades() {
+        double[] clonedGrades = new double[grades.length];
+        for (int i = 0; i < grades.length; i++) {
+            clonedGrades[i] = grades[i];
+        }
+        return clonedGrades;
+    }
+}
