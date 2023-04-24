af480ddd5b6bea5c99988b554394004fb8d0812e
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-04-24 20:08:50 +0400

    Encapsulation code uploaded

---
+++ src/main/java/az/atlacademy/module01/lesson16/encapsulation/RectangleApp.java
@@ -0,0 +1,17 @@
+package az.atlacademy.module01.lesson16.encapsulation;
+
+public class RectangleApp {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(3, 4);
+        System.out.println(rectangle1.getA());
+        System.out.println(rectangle1.getB());
+
+        Rectangle rectangle2 = new Rectangle(1, 2);
+        System.out.println(rectangle2.getA());
+        System.out.println(rectangle2.getB());
+        rectangle2.setA(5);
+        rectangle2.setB(0);
+        System.out.println(rectangle2.getA());
+        System.out.println(rectangle2.getB());
+    }
+}
