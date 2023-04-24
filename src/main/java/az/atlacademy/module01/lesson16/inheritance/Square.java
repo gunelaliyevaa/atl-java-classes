eb62a49efc796088544b3f8cc0212731d364d828
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-04-24 20:13:06 +0400

    Inheritance code uploaded

---
+++ src/main/java/az/atlacademy/module01/lesson16/inheritance/Square.java
@@ -0,0 +1,29 @@
+package az.atlacademy.module01.lesson16.inheritance;
+
+public class Square {
+    private double a;
+    private double area;
+    private double perimeter;
+
+    public Square(double a) {
+        this.a = a;
+        this.area = a * a;
+        this.perimeter = a * 4;
+    }
+
+    public void draw() {
+        System.out.println("Done!");
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+}
