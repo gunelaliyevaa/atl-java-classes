b49ef8827508f30d4efe02631a3a7d8e8ace09b9
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-04-24 21:09:33 +0400

    Instruments created

---
+++ src/main/java/az/atlacademy/module01/lesson19/Instruments/Camera.java
@@ -0,0 +1,7 @@
+package az.atlacademy.module01.lesson19.Instruments;
+
+public class Camera {
+    public static void shoot() {
+        System.out.println("Picture Shot!");
+    }
+}
---
+++ src/main/java/az/atlacademy/module01/lesson19/Instruments/Instruments.java
@@ -0,0 +1,5 @@
+package az.atlacademy.module01.lesson19.Instruments;
+
+public abstract class Instruments {
+    public abstract void play();
+}
