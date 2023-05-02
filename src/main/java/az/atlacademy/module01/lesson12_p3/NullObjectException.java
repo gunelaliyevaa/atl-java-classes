9aff961e385848acb0007edbb60af5869d1054c1
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-05-02 19:29:08 +0400

    NullObjectException created

--- src/main/java/az/atlacademy/module01/lesson12_p3/NullObjectException.java
+++ src/main/java/az/atlacademy/module01/lesson12_p3/NullObjectException.java
@@ -5,29 +5,21 @@ import java.util.Scanner;
 
 public class NullObjectException {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String string = scan.toString();
-        Integer integer = 5;
-        Person person = new Person("Theodore");
-
-        checkString(string);
-        checkPerson(person);
-        checkInteger(integer);
     }
 
-    public static void checkString(String string) {
+    public static void validate(String string) {
         if (string == null) {
             throw new InvalidObjectException("Invalid Input!");
         }
     }
 
-    public static void checkPerson(Person person) {
+    public static void validate(Person person) {
         if (person == null) {
             throw new InvalidObjectException("Invalid Input!");
         }
     }
 
-    public static void checkInteger(Integer integer) {
+    public static void validate(Integer integer) {
         if (integer == null) {
             throw new InvalidObjectException("Invalid Input!");
         }
