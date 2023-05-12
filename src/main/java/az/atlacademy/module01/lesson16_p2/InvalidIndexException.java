ef062a6915c7e0bb73fb6366e366b68fe96c3f12
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-05-12 20:54:06 +0400

    InvalidIndexException created

---
+++ src/main/java/az/atlacademy/module01/lesson16_p2/InvalidIndexException.java
@@ -0,0 +1,7 @@
+package az.atlacademy.module01.lesson16_p2;
+
+public class InvalidIndexException extends RuntimeException{
+    public InvalidIndexException(String message) {
+        super(message);
+    }
+}
