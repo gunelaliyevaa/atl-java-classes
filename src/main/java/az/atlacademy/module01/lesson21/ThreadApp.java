aa7d3e59c628eb981707912a3503ee3736966da5
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-06-01 20:26:12 +0400

    ThreadApp created

--- src/main/java/az/atlacademy/module01/lesson21/ThreadApp.java
+++ src/main/java/az/atlacademy/module01/lesson21/ThreadApp.java
@@ -7,15 +7,39 @@ public class ThreadApp {
         new MyThread().start();
         new MyThread().start();
         new MyThread().start();
+
+        new Thread(new MyThread2()).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Thread ID: " + Thread.currentThread().getId());
+
+            }
+        }).start();
+
+        new Thread(() -> System.out.println("Thread ID: " + Thread.currentThread().getId())).start();
+
     }
 
     public static class MyThread extends Thread {
+
         @Override
         public void run() {
             for (int i = 0; i < 3; i++) {
-                System.out.println("Thread name: " + Thread.currentThread().getName());
+                System.out.println("Thread Name: " + Thread.currentThread().getName());
+            }
+        }
+
+    }
+
+    public static class MyThread2 implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Thread ID: " + Thread.currentThread().getId());
             }
         }
     }
 
 }
+
