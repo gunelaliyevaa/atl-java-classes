ccc1a74f5c42b1510739c1302cd691fdbf8410d2
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-06-01 19:22:24 +0400

    ThreadApp created

---
+++ src/main/java/az/atlacademy/module01/lesson20_p2/ThreadApp.java
@@ -0,0 +1,10 @@
+package az.atlacademy.module01.lesson20_p2;
+
+public class ThreadApp {
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+
+}
