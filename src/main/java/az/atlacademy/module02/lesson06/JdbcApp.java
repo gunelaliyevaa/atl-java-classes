8673e6de1479ba407d78dbb1f1c2108e3c9f87d6
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-07-17 20:20:45 +0400

    JdbcApp created

--- src/main/java/az/atlacademy/module02/lesson06/JdbcApp.java
+++ src/main/java/az/atlacademy/module02/lesson06/JdbcApp.java
@@ -11,27 +11,27 @@ public class JdbcApp {
     public static final String getAllReadersSql = "SELECT * FROM reader;";
 
     public static void main(String[] args) {
-        for (Readers readers : getAllReaders()) {
+        for (Reader readers : getAllReaders()) {
             System.out.println(readers.toString());
         }
     }
 
-    public static List<Readers> getAllReaders() {
+    public static List<Reader> getAllReaders() {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "mysecretpassword")) {
             PreparedStatement query = connection.prepareStatement(getAllReadersSql);
             ResultSet resultSet = query.executeQuery();
-            List<Readers> readers = new ArrayList<>();
+            List<Reader> readers = new ArrayList<>();
             while (resultSet.next()) {
-                String first_name = resultSet.getString("first_name");
-                String last_name = resultSet.getString("last_name");
-                BigDecimal phone_number = resultSet.getBigDecimal("phone_number");
-                readers.add(new Readers(first_name, last_name, phone_number));
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                BigDecimal phoneNumber = resultSet.getBigDecimal("phone_number");
+                readers.add(new Reader(firstName, lastName, phoneNumber));
             }
             return readers;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
         return Collections.emptyList();
     }
+
 }
